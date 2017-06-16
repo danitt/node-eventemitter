bb00e8a03102d8543eca2cb73c284ad4145caa3e
dan <dan@danitt.com>
2017-06-16 02:00:25 +0200

    💥 scaffolding

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
+import { getTemperature, getHumidity, getPressure } from './mocks'
+const EventEmitter = require('events')
+
+interface weatherMeasurements {
+  temperature: number,
+  humidity: number,
+  pressure: number
+}
+
+function measurementsChanged() {
+  // call WeatherData
+}
+
+class WeatherData extends EventEmitter {
+  // set a timer to randomly emit a weather event
+  // setInterval -> measurementsChanged()
+}
+
+function currentConditionsDisplay(data: weatherMeasurements) {}
+function statisticsDisplay(data: weatherMeasurements) {}
+function forecastDisplay(data: weatherMeasurements) {}
+
+
 console.log('Hello .. world?')
---
+++ src/mocks.ts
@@ -0,0 +1,11 @@
+export function getTemperature() {
+
+}
+
+export function getHumidity() {
+
+}
+
+export function getPressure() {
+
+}
